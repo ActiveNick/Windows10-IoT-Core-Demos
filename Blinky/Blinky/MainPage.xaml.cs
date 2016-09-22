dbf292670c9d0a5112d66cdda24678b07834a463
Nick Landry <nlandry@activenick.net>
2016-09-21 21:09:29 -0400

    Added an APIInformation check and more comments

--- Blinky/Blinky/MainPage.xaml.cs
+++ Blinky/Blinky/MainPage.xaml.cs
@@ -14,20 +14,20 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Gpio;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Blinky
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// A Blinky project is the maker edition of "Hello World". It's a trivial project,
+    /// but it does validate that you hardware, IDE and SDKs are setup properly.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-
+        // The pin # represents the GPIO #, *NOT* the physical pin count on the board
         private const int LED_PIN = 5;
         private GpioPin pin;
         private GpioPinValue pinValue;
 
+        // Color brushes used for the on-screen UI only
         private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
         private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
 
@@ -40,24 +40,35 @@ namespace Blinky
 
         private void InitGPIO()
         {
-            var gpio = GpioController.GetDefault();
+            // Before initialize the GPIO controller in code, make sure to add the
+            // Windows IoT Extensions for the UWP in your project references
 
-            if (gpio == null)
+            // Next, we check to see if the GpioController exists on the current execution platform
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Devices.Gpio.GpioController"))
             {
-                pin = null;
-                lblStatus.Text = "There is no GPIO controller on this device.";
-                return;
+                var gpio = GpioController.GetDefault();
+
+                // Let's make sure there actually is a GPIO controller
+                if (gpio == null)
+                {
+                    pin = null;
+                    lblStatus.Text = "There is no GPIO controller on this device.";
+                    return;
+                }
+
+                pin = gpio.OpenPin(LED_PIN);
             }
 
-            pin = gpio.OpenPin(LED_PIN);
-
+            // No GPIO, no pin, then we exit, we're obviously not on an IoT device
             if (pin == null)
             {
                 return;
             }
 
+            // GPIO pins can be used as input or output. Writing to an LED requies and output
             pin.SetDriveMode(GpioPinDriveMode.Output);
-            pinValue = GpioPinValue.Low;
+            // Turn off the LED by default by seting a low voltage (0V) on the pin
+            pinValue = GpioPinValue.Low; 
             pin.Write(pinValue);
         }
 
@@ -65,13 +76,13 @@ namespace Blinky
         {
             if (pinValue == GpioPinValue.High)
             {
-                pinValue = GpioPinValue.Low;
+                pinValue = GpioPinValue.Low; // No voltage, or 0V
                 pin.Write(pinValue);
                 LED.Fill = grayBrush;
             }
             else
             {
-                pinValue = GpioPinValue.High;
+                pinValue = GpioPinValue.High; // Typically 5v or 3.3V depending on the device
                 pin.Write(pinValue);
                 LED.Fill = redBrush;
             }
